350983ec0d16e2509be93946b509628ae42b685a
Tsing <l875383221@gmail.com>
2019-04-02 11:02:46 +0800

    Update 07_reverse_integer.cpp

--- 07_reverse_integer.cpp
+++ 07_reverse_integer.cpp
@@ -3,7 +3,7 @@ public:
     int reverse(int x) {
         double ret = 0;
         if(x == INT_MIN) return 0;
-        int tmp = abs(x), remainder, cnt = 0;
+        int tmp = abs(x), remainder;
         while(tmp != 0){
             remainder = tmp % 10;
             ret = remainder + ret * 10;
