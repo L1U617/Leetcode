f7b2e543666cd805db70e4c82319befa3994a9ca
Tsing <l875383221@gmail.com>
2020-01-03 13:16:17 +0800

    Update 207_course_schedual.py

--- 207_course_schedual.py
+++ 207_course_schedual.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
+        '''
         G = [[] for row in range(numCourses)]
         for i in prerequisites:
             G[i[0]].append(i[1])
@@ -26,3 +27,33 @@ class Solution(object):
                 return False
         
         return True
+        '''
+        """
+        update
+        """
+        G = [[] for i in range(numCourses)]
+        Q = []
+        inDegree = [0 for i in range(numCourses)]
+        
+        for pair in prerequisites:
+            G[pair[0]].append(pair[1])
+            inDegree[pair[1]] += 1
+        count = 0
+        for i in range(numCourses):
+            if inDegree[i] == 0:
+                Q.append(i)
+                count += 1
+        
+        while len(Q) > 0:
+            v = Q[0]
+            del(Q[0])
+            for adjacent in G[v]:
+                inDegree[adjacent] -= 1
+                if inDegree[adjacent] == 0:
+                    Q.append(adjacent)
+                    count += 1
+        
+        if count == numCourses:
+            return True
+        else:
+            return False
