0a25ae7fb16829b2ff9a5b269120aa22cd8e3cdd
Tsing <l875383221@gmail.com>
2020-02-02 10:42:11 +0800

    Create 014_longest_common_prefix.py

---
+++ 014_longest_common_prefix.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs) == 0:
+            return ""
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            j = 0
+            while j < min(len(prefix), len(strs[i])):
+                if prefix[j] != strs[i][j]:
+                    break
+                j += 1
+            prefix = strs[i][0: j]
+        
+        return prefix
